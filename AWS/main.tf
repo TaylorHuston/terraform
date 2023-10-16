047d3735d6b9aa4c9cb55a6b55987fa90b6c170c
Taylor Huston <taylor.a.huston@gmail.com>
2023-10-16 11:10:52 -0400

    Add VPC and subnet

---
+++ AWS/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block           = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support   = true
+    tags = {
+        Name = "dev"
+    }
+}
+
+resource "aws_subnet" "my_public_subnet" {
+    vpc_id                  = aws_vpc.my_vpc.id
+    cidr_block              = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone       = "us-west-2a" 
+
+    tags = {
+      Name = "dev-public"
+    }
+}
