0e4cb494d433481151a17488dfd1aae09f25a606
Taylor Huston <taylor.a.huston@gmail.com>
2023-10-16 11:19:52 -0400

    Internet gateway and route table

--- AWS/providers.tf
+++ AWS/providers.tf
@@ -8,8 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region                   = "us-west-2"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "vscode-terraform"
 }
-
