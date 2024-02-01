e65758f4a4e83007e78f5a023eaa53b50f8cd7ef
Taylor Huston <taylor.a.huston@gmail.com>
2024-01-31 19:10:15 -0500

    Migrate to remote s3 backend

--- AWS/beginner-to-pro/main.tf
+++ AWS/beginner-to-pro/main.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "s3" {
+    bucket         = "example-tf-web-app"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
